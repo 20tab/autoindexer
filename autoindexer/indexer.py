a43bc7ebb066e0ee0c3a2212fd1069f514298732
frank2411 <francesco.perna90@gmail.com>
2014-08-18 16:45:44 +0200

    ultime modifiche autoindexer

--- autoindexer/indexer.py
+++ autoindexer/indexer.py
@@ -4,53 +4,104 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import os
 import jinja2
+import sys
+import getopt
+import glob
 
-INDEX = 'index.html'
-BASE = '/Users/twentytab/projects/autoindexer'
+
+arguments = "d:i:t:D:"
+
+long_arguments = [
+    "directory=",
+    "index-name=",
+    "index-template=",
+    "depth="
+]
+
+opts, extraparams = getopt.getopt(sys.argv[1:], arguments, long_arguments)
+
+args = dict(opts)
+
+INDEX = args["-i"] if "-i" in args else 'index.html'
+INDEX_TEMPLATE = args["-t"] if "-t" in args else 'index.jinja'
+BASE = os.path.abspath(args["-d"])
+DEPTH = int(args["-D"]) if "-D" in args else None
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath='/'))
 template = env.get_template(
-    os.environ['HOME'] + '/projects/autoindexer/index.jinja'
+    BASE + '/' + INDEX_TEMPLATE
 )
 
-print template
+print "DEPTH ", DEPTH
 
 
-def generate(dirname):
+class Item():
+
+    def __init__(self):
+        self.is_dir = False
+        self.path = ""
+        self.name = ""
+        self.base = ""
+
+    def __unicode__(self):
+        self.path
+
+
+def generate(dirname, depth=None):
+
     items = []
-    directories = []
-    files = []
-    print BASE
     base = dirname[len(BASE):]
-    print "aaaaaaa", base
+
     for item in os.listdir(dirname):
-        if item.startswith('.'):
+
+        if (
+            item.startswith('.') or
+            item.endswith('.ini') or
+            item == INDEX or
+            item == INDEX_TEMPLATE or
+            item == "indexer.py"
+        ):
             continue
-        path = os.path.join(dirname, item)
-        items.append(item)
-        if os.path.isdir(path):
-            generate(path)
-    items.sort()
-    directories.sort()
-    files.sort()
+
+        item_obj = Item()
+        item_obj.name = item
+        item_obj.path = os.path.join(dirname, item)
+        item_obj.base = base
+        items.append(item_obj)
+
+        if os.path.isdir(item_obj.path):
+            item_obj.is_dir = True
+            if depth != 0:
+                current_depth = depth
+                if depth:
+                    current_depth = depth-1
+
+                generate(item_obj.path, current_depth)
+
     html = template.render(
-        {'base': base, 'dirname': dirname, 'items': items,
-         'directories': directories, 'files': files})
+        {
+            'base': base,
+            'dirname': dirname,
+            'items': items,
+        }
+    )
+
     index = os.path.join(dirname, INDEX)
+
     with open(index, 'w') as f:
         f.write(html)
 
 
-class MyHandler(FileSystemEventHandler):
+class IndexerHandler(FileSystemEventHandler):
+
     def on_modified(self, event):
-        print "MODIFIED"
-        generate(BASE)
+        generate(BASE, DEPTH)
 
 if __name__ == "__main__":
-    generate(BASE)
-    event_handler = MyHandler()
+    generate(BASE, DEPTH)
+    event_handler = IndexerHandler()
     observer = Observer()
-    observer.schedule(event_handler, path='.', recursive=False)
+    observer.schedule(event_handler, path=BASE, recursive=False)
     observer.start()
 
     try:
